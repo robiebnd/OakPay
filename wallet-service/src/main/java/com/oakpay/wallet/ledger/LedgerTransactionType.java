ddf8221fe40eda6447342d2430a5af8cebf6a26f
Robson Banda <138120031+robiebnd@users.noreply.github.com>
2026-09-02 14:15:43 +0200

    Add ledger transaction types

---
+++ wallet-service/src/main/java/com/oakpay/wallet/ledger/LedgerTransactionType.java
@@ -0,0 +1,11 @@
+package com.oakpay.wallet.ledger;
+
+public enum LedgerTransactionType {
+    DEPOSIT,
+    WITHDRAWAL,
+    TRANSFER_IN,
+    TRANSFER_OUT,
+    TRADE_BUY,
+    TRADE_SELL,
+    FEE
+}
