37de354a38e1bcec3f9ed489b60fe022a8bb440c
Robson Banda <138120031+robiebnd@users.noreply.github.com>
2026-09-03 15:03:43 +0200

    feat(p2p): enable scheduled trade expiry

--- trading-service/src/main/java/com/oakpay/trading/TradingServiceApplication.java
+++ trading-service/src/main/java/com/oakpay/trading/TradingServiceApplication.java
@@ -2,8 +2,10 @@ package com.oakpay.trading;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class TradingServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(TradingServiceApplication.class, args);
