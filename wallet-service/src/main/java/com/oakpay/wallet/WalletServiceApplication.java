0e3aa1476b2d993a93ae94c96f41b26e7a6676c7
Robson Banda <138120031+robiebnd@users.noreply.github.com>
2026-09-20 00:16:05 +0200

    feat(wallet): enable reconciliation scheduling

--- wallet-service/src/main/java/com/oakpay/wallet/WalletServiceApplication.java
+++ wallet-service/src/main/java/com/oakpay/wallet/WalletServiceApplication.java
@@ -2,8 +2,10 @@ package com.oakpay.wallet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class WalletServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(WalletServiceApplication.class, args);
